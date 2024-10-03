89063610a368fec30573db7828df7ff124091732
Frame <2boylc17@solent.ac.uk>
2024-10-03 15:58:19 +0100

    3

---
+++ src/ex2.kt
@@ -0,0 +1,7 @@
+fun main () {
+    println("What is your name?")
+    val n = readlnOrNull()
+    for (i in 1..10) {
+        println(n)
+    }
+}
---
+++ src/music.kt
@@ -0,0 +1,14 @@
+fun main () {
+    val artists = arrayOf("a","b","c")
+    println("Guess the artists")
+    while (true) {
+        val n = readlnOrNull()
+        if (n in artists){
+            return(println("Correct"))
+        }
+        else {
+            println("Try Again")
+        }
+    }
+
+}
